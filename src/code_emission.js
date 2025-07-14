1c2f5ac077ef25cda2fa6c540018b950ca75f604
DaiF <julie.fiadino@gmail.com>
2025-07-14 20:34:42 +0200

    refacto: polish up shader code output

--- src/code_emission.js
+++ src/code_emission.js
@@ -1,9 +1,9 @@
 function hexToRgb(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
+    r: (parseInt(result[1], 16) / 255).toFixed(2),
+    g: (parseInt(result[2], 16) / 255).toFixed(2),
+    b: (parseInt(result[3], 16) / 255).toFixed(2)
   } : null;
 }
 
@@ -12,13 +12,13 @@ let var_count = 0;
 function emitOutput(node) {
     const colorValue = hexToRgb(document.getElementById(node.attrib_ids[0]).value);
 
-    let color = `vec4(${colorValue.r / 255}, ${colorValue.g / 255}, ${colorValue.b / 255}, 1.0);`;
+    let color = `vec4(${colorValue.r}, ${colorValue.g}, ${colorValue.b}, 1.0);`;
     let shaderChildren = "";
 
     if (node.attrib_links['out-color'])
         [shaderChildren, color] = emitNode(node.attrib_links['out-color']);
 
-    const shaderContent = `out_color = ${color};\n`
+    const shaderContent = `  out_color = ${color};\n`
     return [shaderChildren + shaderContent, ""];
 }
 
@@ -37,11 +37,11 @@ function emitColorRamp(node) {
     const weightVal = `weight_${var_count++}`;
     const outVal = `color_${var_count++}`;
 
-    let shaderContent = `vec4 ${startVal} = vec4(${startColor.r / 255}, ${startColor.g / 255}, ${startColor.b / 255}, 1.0);\n`;
-    shaderContent += `vec4 ${endVal} = vec4(${endColor.r / 255}, ${endColor.g / 255}, ${endColor.b / 255}, 1.0);\n`;
+    let shaderContent = `  vec4 ${startVal} = vec4(${startColor.r}, ${startColor.g}, ${startColor.b}, 1.0);\n`;
+    shaderContent += `  vec4 ${endVal} = vec4(${endColor.r}, ${endColor.g}, ${endColor.b}, 1.0);\n`;
 
-    shaderContent += `float ${weightVal} = ${weight};\n`;
-    shaderContent += `vec4 ${outVal} = mix(${startVal}, ${endVal}, ${weightVal});\n`;
+    shaderContent += `  float ${weightVal} = ${weight};\n`;
+    shaderContent += `  vec4 ${outVal} = mix(${startVal}, ${endVal}, ${weightVal});\n`;
 
     return [shaderContent, outVal];
 }
