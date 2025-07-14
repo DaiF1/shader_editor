87ff99273a98c85f5472c30abd552d360c63b2b1
DaiF <julie.fiadino@gmail.com>
2025-07-14 13:39:06 +0200

    feat: basic line draw and update

--- src/node_canvas.js
+++ src/node_canvas.js
@@ -1,48 +1,56 @@
 import { generateShaderCode } from "./code_emission";
 import { nodeSpecs } from "./nodes";
 
+import LeaderLine from "leader-line-new";
+
 const container = document.getElementById("node-canvas");
 const contextMenu = document.getElementById("add-panel");
 let id = 0;
 
 let outputNode = undefined;
+const allNodes = [];
 
 const shaderOutput = document.getElementById("code-output");
 
-function dragElement(elmnt) {
-  var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
+function dragElement(node, elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-  }
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+    const title = elmnt.querySelector('.node-title');
+    title.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+
+        for (let link of node.links)
+            link.position();
+    }
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
 
 function updateShaderCode(redrawCallback) {
@@ -84,21 +92,24 @@ function addNode(name, x, y, redrawCallback) {
 
     container.innerHTML += html;
 
-    const nodes = container.querySelectorAll(".node");
-    nodes.forEach(node => {
-        dragElement(node);
-        const inputs = node.querySelectorAll("input");
-        inputs.forEach(input => input.addEventListener("change", () => updateShaderCode(redrawCallback)));
-    });
-
     const out = {
         id: id,
         attrib_ids: attribIds,
-        attri_links: attribLinks,
+        attrib_links: attribLinks,
+        links: [] // List of LeaderLine objects
+    }
+    allNodes.push(out);
+
+    for (let node of allNodes)
+    {
+        const nodeDom = document.getElementById(`node-${node.id}`);
+        dragElement(node, nodeDom);
+
+        const inputs = nodeDom.querySelectorAll("input");
+        inputs.forEach(input => input.addEventListener("change", () => updateShaderCode(redrawCallback)));
     }
 
     id++;
-
     return out;
 }
 
@@ -106,6 +117,15 @@ export function initCanvas(redrawCallback) {
     const x = container.clientWidth / 2;
     const y = container.clientHeight / 2 - 100;
     outputNode = addNode("Output", x, y, redrawCallback);
+    const tex = addNode("Texture", x - 500, y, redrawCallback);
+
+    const start = document.getElementById(`node-${outputNode.id}`);
+    const end = document.getElementById(`node-${tex.id}`);
+    console.log(start, end);
+
+    const line = new LeaderLine(end, start, { color: 'white', startPlug: 'disc', endPlug: 'disc' });
+    allNodes[0].links.push(line);
+    allNodes[1].links.push(line);
 
     container.addEventListener('contextmenu', (event) => {
         contextMenu.style.left = `${event.clientX}px`;
